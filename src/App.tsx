cf243711d873f0be6b0e41080ebd7453f77ee18c
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-26 22:11:28 +0200

    Refactor layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Form from './components/Form';
 function App() {
   return (
     <div className='App'>
-      <main className='w-full lg:w-1/2 m-auto flex flex-col lg:flex-row gap-5 p-2'>
+      <main className='w-full mx-auto flex flex-col-reverse lg:flex-row gap-5 p-2 md:w-1/2'>
         <List />
         <Form />
       </main>
