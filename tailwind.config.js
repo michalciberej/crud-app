373619fd451ec0e9eab4b330a30cb87bfe12dd22
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-26 12:44:43 +0200

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const autoprefixer = require('autoprefixer');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
+
